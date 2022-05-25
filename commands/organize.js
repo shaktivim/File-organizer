5d36b53449b3f03926df3d04ee3e3f250000dfd5
shaktivim <shaktivim@gmail.com>
2022-05-25 19:53:32 +0530

    minor changes

--- commands/organize.js
+++ commands/organize.js
@@ -3,13 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const types = require('./utility.json');
 
-// let types = {
-//     media: ["mp4", "mkv", "mp3"],
-//     archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
-//     documents: ["docx", "doc", "pdf", "xlsx", "xls", "odt", "ods", "odp", "odg", "odf", "txt", "ps", "tex"],
-//     app: ["exe", "dmg", "pkg", "deb"],
-// };
-
 
 function organizeFn(dirpath){
     // console.log("Organize command is passed");
